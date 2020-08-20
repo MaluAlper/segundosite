1fea29a69b16aa29d125611d8dd5a6637be6cc38
Maria Luiza <malualper@hotmail.com>
2020-08-19 23:17:23 -0300

    segundosite

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace segundosite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
